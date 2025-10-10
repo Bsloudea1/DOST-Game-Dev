ee1dec599ba10e4e1b4b56bdc656be4e011b8a43
flrs-25 <caflores@paterostechnologicalcollege.edu.ph>
2025-10-10 18:50:30 +0800

    Scenes were separated

---
+++ Assets/Script/MainMenu-Lobbies.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MainMenu-Lobbies", menuName = "Scriptable Objects/MainMenu-Lobbies")]
+public class MainMenu-Lobbies : ScriptableObject
+{
+    
+}
